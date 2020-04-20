54602adfa93b74c42df5cc1bc2a6038f0a6a70f2
osti <osti@romagnole.com.br>
2020-04-20 16:18:26 -0300

    tollbar curved button

--- XamarinTools/XamarinTools/MainPage.xaml.cs
+++ XamarinTools/XamarinTools/MainPage.xaml.cs
@@ -1,21 +1,49 @@
-﻿using System;
+﻿using FormsCurvedBottomNavigation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinTools.View;
 
 namespace XamarinTools
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : CurvedBottomTabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            this.Children.Add(new PageTesteView("Acontecendo Ao Vivo")
+            {
+                Title = "Ao Vivo",
+                IconImageSource = "aovivo"
+            });
+            this.Children.Add(new PageTesteView("Eventos de Hoje")
+            {
+                Title = "Evento",
+                IconImageSource = "evento"
+            });
+            this.Children.Add(new PageTesteView("Informações")
+            {
+                Title = "Info",
+                IconImageSource = "info"
+            });
+            this.Children.Add(new PageTesteView("Temas de Hoje")
+            {
+                Title = "Temas",
+                IconImageSource = "temas"
+            });
+        }
+
+        private void CurvedBottomTabbedPage_FabIconClicked(object sender, EventArgs e)
+        {
+            //
         }
     }
 }
---
+++ XamarinTools/XamarinTools/View/PageTesteView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinTools.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageTesteView : ContentPage
+    {
+        public PageTesteView(string teste)
+        {
+            InitializeComponent();
+
+            lblPagina.Text = $"Label de teste {teste}";
+        }
+    }
+}
